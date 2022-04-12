adbfc4abea6578353cc62446b532f6bebbd0d131
root <Oluwayemisi>
2022-04-12 14:30:49 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /** 
- * * main - Entry point 
- * 
- *
- * * * Return: nothing, return void 
- * */
+* main - Entry point 
+*
+* Return: nothing, return void 
+*/
 int main(void)
 {
 	_putchar('_');
