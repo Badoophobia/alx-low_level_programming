0525e23aa988619401e5758bb9020692e75e08ab
root <Oluwayemisi>
2022-07-14 14:33:50 -0700

    task commit

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc *.o -fPIC -shared -o liball.so
-LD_PRELOAD=$WPD/liball.so
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +21,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int mul(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
-#endif
+#endif /*MAIN_H*/
