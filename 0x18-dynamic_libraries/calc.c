7ae156173cb1372404e265b9d3521550a57c6f65
root <Oluwayemisi>
2022-07-14 14:43:22 -0700

    task commit

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/*
+ * add - adds number
+ * @a: input
+ * @b: input
+ * Return: returns the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/*
+ * mul -multiplies number
+ * @a: input
+ * @b: input
+ * Return: returns the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/*
+ * sub - substracts  number
+ * @a: input
+ * @b: input
+ * Return: returns the difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/*
+ * mod - adds number
+ * @a: input
+ * @b: input
+ * Return: returns the modula of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/*
+ * div -  number
+ * @a: input
+ * @b: input
+ * Return: returns the division of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
